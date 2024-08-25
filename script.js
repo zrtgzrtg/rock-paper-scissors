e8423fd6514e89b690157cec8abe08af3cf99fdc
zrtg <[thomas4stank@gmail.com]>
2024-08-25 20:38:05 +0200

    Solved everything up to 4

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 
+let humanScore = 0
+let computerScore = 0
 function getComputerChoice() {
+    
+    let computerChoice = ""
     let rndNumber = Math.floor(Math.random()*3)+1
-    return rndNumber
+    
+    if(rndNumber === 1) {
+        computerChoice = "Rock"
+    } else if (rndNumber === 2) {
+        computerChoice = "Paper"
+    } else if (rndNumber === 3) {
+        computerChoice = "Scissors"
+    }
+    return computerChoice
+}
+function getHumanChoice() {
+    let answer = prompt("Choose Rock, Paper or Scissors!")
+    return answer
 }
 console.log(getComputerChoice())
+console.log(getHumanChoice())
